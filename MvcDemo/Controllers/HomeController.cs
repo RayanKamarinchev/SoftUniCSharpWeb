3f3ea7f035ffaebc7c593c6f8357c58f13a6853a
RayanKamarinchev <rayan2008bg@gmail.com>
2023-02-15 16:37:24 +0200

    Intro basic exercises

---
+++ MvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcDemo.Models;
+using System.Diagnostics;
+
+namespace MvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("numbers")]
+        public IActionResult Numbers()
+        {
+            return View();
+        }
+
+        [Route("n_numbers")]
+        public IActionResult NumbersToN(int count = 3)
+        {
+            ViewBag.Count = count;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcDemo/Controllers/ProductsController.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using MvcDemo.Models;
+
+namespace MvcDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00m
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50m
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50m
+            }
+        };
+        public IActionResult All(string keyword)
+        {
+            if (!(keyword is null))
+            {
+                var foundProducts = this.products.Where(pr => pr.Name.ToLower().Contains(keyword.ToLower()));
+                return View(foundProducts);
+            }
+            return View(this.products);
+        }
+
+        public IActionResult ById(int id)
+        {
+            ProductViewModel pr = products.FirstOrDefault(p => p.Id == id);
+            if (pr is null)
+            {
+                return BadRequest();
+            }
+            return View(pr);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            return Json(products, options);
+        }
+
+        public IActionResult AllAsText()
+        {
+            return Content("Average product");
+        }
+
+        public IActionResult AllAsTextFile()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var pr in products)
+            {
+                sb.AppendLine($"Product {pr.Id}: {pr.Name} - {pr.Price:f2}lv");
+            }
+
+            Response.Headers.Add(HeaderNames.ContentDisposition, @"attachment;file=products.txt");
+            return File(Encoding.UTF8.GetBytes(sb.ToString().TrimEnd()), "text/plain");
+        }
+    }
+}
