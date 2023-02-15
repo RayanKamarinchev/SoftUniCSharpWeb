82ef57ff299a68d1e251c409fb0b4215d725ce04
RayanKamarinchev <rayan2008bg@gmail.com>
2023-02-15 18:09:47 +0200

    Text splitter

---
+++ TextSplitter/Controllers/SplitTextController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextSplitter.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TextSplitter.Controllers
+{
+    [Route("text-split")]
+    public class SplitTextController : Controller
+    {
+        public IActionResult Show(TextViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel newText)
+        {
+            newText.SplitText = newText.Text.Replace(" ", "\n");
+            return RedirectToAction("Show", newText);
+        }
+    }
+}
